815330b6ce3681e7dc6d43522f6588ebedbaf3a0
Hristiyan Stefanov <christian.stefanov@outlook.com>
2022-03-13 20:14:06 +0200

    introduced mock user service

--- Workshop-Forum/su-forum/src/app/header/header.component.ts
+++ Workshop-Forum/su-forum/src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from '../user.service';
 
 @Component({
   selector: 'app-header',
@@ -6,10 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+  userService: UserService;
 
-  constructor() { }
+  constructor(userService: UserService) {
+    this.userService = userService;
+  }
 
   ngOnInit(): void {
   }
 
+  onLogin(): void {
+    this.userService.login();
+  }
+
+  onLogout(): void {
+    this.userService.logout();
+  }
 }
